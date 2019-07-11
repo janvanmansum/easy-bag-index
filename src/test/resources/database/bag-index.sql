c3058f0b8f76fee1687bbfe17cd16600fe9e9055
Richard van Heest <rvanheest@users.noreply.github.com>
2019-07-11 13:08:05 +0200

    EASY-1863: expand doi column in bag_info table (#43)

--- src/test/resources/database/bag-index.sql
+++ src/test/resources/database/bag-index.sql
@@ -2,5 +2,5 @@ CREATE TABLE bag_info (
     bagId CHAR(36) NOT NULL PRIMARY KEY,
     base CHAR(36) NOT NULL,
     created CHAR(29) NOT NULL,
-    doi CHAR(36) NOT NULL
+    doi VARCHAR(256) NOT NULL
 );
